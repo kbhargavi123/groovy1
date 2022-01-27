a3065c251ce541bc1fba8105911892b594ac25f8
kbhargavi123 <bharagvik2@gmail.com>
2022-01-27 16:30:48 -0500

    jenkins file added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,22 +16,21 @@ pipeline
         {
             steps {
                 def a=$Type
-               if (a="Google")
-             {
-                def li=a as List
-                println li
-             }
+               if (a == "Google")
+               {
+                  def li=a as List
+                  println li
+               }
                else
-
-             {
-                def map = [:]
-                def b=a.length();
-                println b
-                String[] sp= a.split("(?<=\\G....)");
-                def fname= sp.first()
-                def lname= sp.last()
-                map[fname] = lname
-                println map
+              {
+                  def map = [:]
+                  def b=a.length();
+                  println b
+                  String[] sp= a.split("(?<=\\G....)");
+                  def fname= sp.first()
+                  def lname= sp.last()
+                  map[fname] = lname
+                  println map
               
              
              }
